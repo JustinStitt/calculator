b391dbcdc6fa6a0c1a252d1ca5a5b4fee257c7ce
Vanessa Roque <vroque19@csu.fullerton.edu>
2023-07-22 20:09:00 +0000

    created repo

---
+++ script.js
@@ -0,0 +1,105 @@
+let runningTotal = 0;
+let buffer = 0;
+let previousOperator;
+let screen;
+let buttons;
+let button;
+
+screen = document.querySelector('.screen');
+container = document.querySelector('.calc-buttons');
+buttons = document.querySelectorAll('.calc-button');
+
+
+function buttonClick(value) {
+    isNaN(val) ? handleOp(value) : handleNum(value);
+    screen.innerText = buffer;
+}
+
+function handleOp(operation) {
+    switch(operation) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if(previousOperator === null) {
+                return;
+            }
+            flushOperator(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if(buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer.buffer.toString(0, buffer.length - 1)
+            }
+            break;
+        case '+':
+        case '-':
+        case 'x':
+        case '÷':
+            handleMath(operation);
+            break;
+    }
+}
+
+function handleMath(operation) {
+    if(buffer === '0') {
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        doMath(intBuffer);
+    }
+    previousOperator = operation;
+    buffer = '0';
+}
+
+function doMath() {
+    if(previousOperator === '+') {
+        runningTotal += intBuffer;
+    } 
+    else if(previousOperator === '-') {
+        runningTotal -= intBuffer;
+    }
+    else if(previousOperator === '÷') {
+        runningTotal /= intBuffer;
+    }
+    else if(previousOperator === 'x') {
+        runningTotal *= intBuffer;
+    }
+}
+
+function handleNum(digit) {
+    console.log("digit:", digit)
+    if(buffer === '0') {
+        buffer = digit;
+    } else {
+        buffer += digit;
+    }
+}
+
+function init() {
+    buttons.addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    })
+}
+
+buttons.forEach(button => {
+var currentColor = button.style.backgroundColor;
+button.addEventListener('mousedown', () => {
+    const bgColor = window.getComputedStyle(container).backgroundColor;
+    button.style.backgroundColor = bgColor;
+});
+button.addEventListener('mouseup', () => {
+    button.style.backgroundColor = currentColor;
+})
+});
+
+init();
